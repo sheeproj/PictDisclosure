9b2cc83bcdc3a25ed88cd1e82c70fe6fa15460a4
yang <yooo.ichiro@gmail.com>
2018-03-31 00:00:53 +0900

    update

--- dragdrop.js
+++ dragdrop.js
@@ -1,23 +1,27 @@
 document.ondragover = document.ondrop = function(e) {
-    e.preventDefault(); // イベントの伝搬を止めて、アプリケーションのHTMLとファイルが差し替わらないようにする
+    e.preventDefault();
     return false;
 };
+
 var holder = document.getElementById('holder');
-/** hoverエリアにドラッグされた場合 */
 holder.ondragover = function () {
     return false;
 };
-/** hoverエリアから外れた or ドラッグが終了した */
 holder.ondragleave = holder.ondragend = function () {
     return false;
 };
-/** hoverエリアにドロップされた */
 holder.ondrop = function (e) {
-    e.preventDefault(); // イベントの伝搬を止めて、アプリケーションのHTMLとファイルが差し替わらないようにする
+    e.preventDefault();
 
     var file = e.dataTransfer.files[0];
-    holder.innerText = file.name;
+
+    var title = document.getElementById('title');
+
+    title.innerText = 'PictDisclosure : ' + file.name;
     console.log(file.path);
+
+    var image = document.getElementById('image');
+    image.src = file.path;
     
     return false;
 };
